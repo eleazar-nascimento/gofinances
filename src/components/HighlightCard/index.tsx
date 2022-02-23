068ed721ac43af929829059696aafaa8aa26a5a8
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2022-02-22 22:48:19 -0300

    feat: Highlight Cards Creation

---
+++ src/components/HighlightCard/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { 
+    Container,
+    Header,
+    Title,
+    Icon,
+    Footer,
+    Amount,
+    LastTransaction,
+} from "./styles";
+
+export function HighlightCard() {
+  return (
+    <Container>
+      <Header>
+        <Title>Entradas</Title>
+        <Icon name="arrow-up-circle" />
+      </Header>
+      <Footer>
+        <Amount>R$ 17.400,00</Amount>
+        <LastTransaction>ultoima entrada dia 14 de abril</LastTransaction>
+      </Footer>
+    </Container>
+  );
+}
